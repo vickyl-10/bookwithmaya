da9a868a1439caaf62cf358a03c308a5f227e40c
Vicky Lee <vickylee@Vicky-L-Computer.local>
2025-07-30 13:52:06 -0400

    Initial commit for Vercel deployment: add all project files and lock dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
